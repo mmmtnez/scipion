34a43bb8cf322300a215c55bb8c0bcbb8787c41f
mmmtnez <mmmtnez@cnb.csic.es>
2018-09-07 11:03:38 +0200

    Format correction of Help section

--- pyworkflow/em/packages/chimera/protocol_modeller_search.py
+++ pyworkflow/em/packages/chimera/protocol_modeller_search.py
@@ -63,27 +63,31 @@ class ChimeraModelFromTemplate(ChimeraProtBase):
                       label='PDBx/mmCIF file template',
                       help="PDBx/mmCIF file template used as basic atomic "
                            "structure to model your specific sequence.")
+        form.addParam('inputVolume', PointerParam, pointerClass="Volume",
+                      condition='False',
+                      label='Input Volume', allowsNull=True,
+                      help="Volume to process")
         form.addSection(label='Help')
         form.addLine('''Step 1: In Chimera main menu, select Tools -> Sequence 
         -> Sequence; Select the specific chain as template to model your 
         sequence and press Show. The sequence of the selected chain will be 
-        showed in an individual window.\nStep 2: In the sequence window menu, 
+        showed in an individual window.\n\nStep 2: In the sequence window menu, 
         select Edit -> Add sequence. A new window will be opened in which you 
         can paste the specific sequence that you want to model (Plain text) 
         or download it from a text file (From File), or download it from 
         UniProt by using the UniProt name/ID (From UniProt). Once selected 
         the sequence, press OK. This last sequence will appear aligned to the 
-        template's sequence.\nStep 3: In the sequence window menu, select 
+        template's sequence.\n\nStep 3: In the sequence window menu, select 
         Structure -> Modeller (homology)...; A new window for Comparative 
         Modeling with Modeller will appear. Select your specific sequence as 
         the sequence to be modeled, and the input atomic structure used as 
         template for modeling. Select Run Modeller via web service and write 
         the Modeller license key supplied. Finally, press OK.\nWAITING TIME: 
-        (In chimera main window, lower left corner you may see the status of 
-        your job)\nStep 4: When finished click quit, 5 models will appear. 
-        Choose the one you like best. Save it as first guess in Scipion by 
-        executing the Chimera command *scipionwrite [model #n]* 
-        (Chimera main menu: Favorites -> Command Line). 
+        (you may see the status of your job in chimera main window, 
+        lower left corner.)\n\nStep 4: When finished click quit, 5 models 
+        will appear. Choose the one you like the best. Save it as first guess 
+        in Scipion by executing the Chimera command *scipionwrite [model #n]* 
+        (Chimera main menu: Favorites -> Command Line).\n 
         When you use the command line scipionwrite, the Chimera session will 
         be saved by default. Additionally, you can save the Chimera session 
         whenever you want by executing the command *scipionss". You will be 
